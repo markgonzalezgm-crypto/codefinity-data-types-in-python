fdb28a3bd267739370862d25debcdc7ce291bc12
markgonzalezgm-crypto <>
2026-08-19 23:11:45 +0000

    Substring search

---
+++ strings/substring_search/main.py
@@ -0,0 +1,14 @@
+string = "Work your way up!"
+
+# Find the index of the letter `p`
+idx_p = string.find("p")
+
+# Find the index of the letter `y`
+idx_y = string.find('y')
+
+# Find the index of the letter `r`
+idx_r = string.find("r")
+
+print("Variable p equals:", idx_p)
+print("Variable y equals:", idx_y)
+print("Variable r equals:", idx_r)
